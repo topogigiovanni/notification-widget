6f4e23a7d629d62a6890264708cdd9abcb41bbe2
giovanni.mendes <giovanni.mendes@dcg.com.br>
2018-02-08 13:27:00 -0200

    https://trello.com/c/Is76LSPW/8-definir-lib-de-crypto-client-e-server-para-passar-par%C3%A2metros-para-api-com-seguran%C3%A7a

--- static/widget.js
+++ static/widget.js
@@ -10,7 +10,6 @@
 	const debug = !!DCG.debug;
 
 	let Deferred;
-	let sha1 = {}
 	let ajax = {}
 	let state = {}
 	let sessionData = {}
@@ -43,8 +42,7 @@
 			}
 
 			return _services[name];
-		},
-		getSha: () => sha1
+		}
 	}
 
 	// ** Services
@@ -68,41 +66,20 @@
 				}
 				data.url = self._buildUrl(data.url)
 				data.dataType = 'json';
+				data.headers = data.headers || {};
+				data.headers['X-DCG-Notify-Trusted'] = 'true';
 
-				let _data = data.data || {};
+				let _data = data.data || sessionData;
 
-				// data.data = {
-				// 	data: sha1(JSON.stringify(_data))
-				// }
-
-				data.data = `d=${sha1(JSON.stringify(_data))}`
-
-				log('data', data)
+				if (_data) {
+					data.data = `d=${_encode(_data)}`
+				}
 
 				var req = self._requestCache[name] = ajax(data);
 
 				return req;
-				/*
-					.then((r) => {
-						self._requestCache[name] = null;
-						return r;
-					})
-					.catch((er) => {
-						self._requestCache[name] = null;
-						return er;
-					});*/
 			},
 			getNotifications: function() {
-				let self = this;
-				// return ajax({
-				// 		url: self._buildUrl('api/posts/published'),
-				// 		success: (r) => {
-				// 			console.log('r success', r);
-				// 		}
-				// 	})
-				// 	.then((r) => {
-				// 		console.log('r then', r);
-				// 	});
 				return this._buildRequest('getNotifications', {
 					url: 'api/widget/data'
 				});
@@ -206,6 +183,10 @@
 		console.log.apply(console, arguments)
 	}
 
+	function _encode(object) {
+		return encodeURIComponent(window.btoa(JSON.stringify(object)))
+	}
+
 	function _buildUrl(url) {
 		let host = debug ? 'http://localhost:3001/' : 'http://localhost:3001/';
 
@@ -213,14 +194,46 @@
 	}
 
 	function _polyfills() {
-
-		// addEventListener
+		// btoa
 		(function(root) {
 
-			if (!root.addEventListener && root.attachEvent) {
+			if (!root.btoa) {
 
-				root.addEventListener = function(event, callback) {
-					return root.attachEvent(event, callback);
+				var b64 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=',
+					b64re = /^(?:[A-Za-z\d+\/]{4})*?(?:[A-Za-z\d+\/]{2}(?:==)?|[A-Za-z\d+\/]{3}=?)?$/;
+
+				root.btoa = function(string) {
+
+					string = String(string);
+
+					var bitmap,
+						a,
+						b,
+						c,
+						result = '',
+						i = 0,
+						rest = string.length % 3; // To determine the final padding
+
+					for (; i < string.length;) {
+
+						if (
+							(a = string.charCodeAt(i++)) > 255 ||
+							(b = string.charCodeAt(i++)) > 255 ||
+							(c = string.charCodeAt(i++)) > 255
+						) {
+
+							throw new TypeError('Failed to execute "btoa" on "Window": The string to be encoded contains characters outside of the Latin1 range.');
+
+						}
+
+						bitmap = (a << 16) | (b << 8) | c;
+
+						result += b64.charAt(bitmap >> 18 & 63) + b64.charAt(bitmap >> 12 & 63) + b64.charAt(bitmap >> 6 & 63) + b64.charAt(bitmap & 63);
+
+					}
+
+					return rest ? result.slice(0, rest - 3) + '==='.substring(rest) : result;
+
 				};
 
 			}
@@ -287,217 +300,6 @@
 			}
 
 		}(window));
-
-	}
-
-	function _registerSha1() {
-		function hex(val) {
-			if (val >>> 0 !== val) {
-				return "  ";
-			}
-			if (val < 0x10) {
-				return "0" + val.toString(16);
-			}
-			return val.toString(16);
-		}
-
-		function ensureArray(buffer) {
-			if (buffer instanceof Uint8Array) {
-				return buffer;
-			}
-			if (buffer instanceof ArrayBuffer || Array.isArray(buffer)) {
-				return new Uint8Array(buffer);
-			}
-			if (buffer.buffer instanceof ArrayBuffer) {
-				return new Uint8Array(buffer.buffer);
-			}
-			throw new TypeError("Invalid buffer type " + buffer);
-		}
-		//https://github.com/hookflash/obsolete.cifre/blob/master/sha1.js
-		(
-			(function(m) {
-				let _sha1 = m()
-
-				_sha1.tohex = function tohex(array) {
-					var string = "";
-					array = ensureArray(array);
-					for (var i = 0, l = array.length; i < l; i++) {
-						string += hex(array[i]);
-					}
-					return string;
-				}
-
-				_sha1.stringToArray = function stringToArray(string) {
-					// UTF-8 encode the string using one character per byte
-					string = unescape(encodeURIComponent(string));
-					var length = string.length;
-					var arr = new Array(length);
-					for (var i = 0; i < length; i++) {
-						arr[i] = string.charCodeAt(i);
-					}
-					return arr;
-				}
-
-				sha1 = (input) => {
-					return _sha1.tohex(_sha1(_sha1.stringToArray(input)));
-				}
-			})
-		)(function() {
-			"use strict";
-
-			var state = new Uint32Array(5);
-			var bstate = new Uint8Array(state.buffer);
-
-			// Create a buffer for each 80 word block.
-			var block = new Uint32Array(80);
-
-			function common(a, e, w, k, f) {
-				return (f + e + w + k + ((a << 5) | (a >>> 27))) >>> 0;
-			}
-
-			function f1(a, b, c, d, e, w) {
-				return common(a, e, w, 0x5A827999, d ^ (b & (c ^ d)));
-			}
-
-			function f2(a, b, c, d, e, w) {
-				return common(a, e, w, 0x6ED9EBA1, b ^ c ^ d);
-			}
-
-			function f3(a, b, c, d, e, w) {
-				return common(a, e, w, 0x8F1BBCDC, (b & c) | (d & (b | c)));
-			}
-
-			function f4(a, b, c, d, e, w) {
-				return common(a, e, w, 0xCA62C1D6, b ^ c ^ d);
-			}
-
-			// function hex8(num) {
-			//   var hex = num.toString(16).toUpperCase();
-			//   return "00000000".substr(hex.length) + hex;
-			// }
-
-			function cycle(state, block) {
-				var a = state[0],
-					b = state[1],
-					c = state[2],
-					d = state[3],
-					e = state[4];
-
-				var fn = f1;
-				for (var i = 0; i < 80; i += 5) {
-					if (i === 20) {
-						fn = f2;
-					} else if (i === 40) {
-						fn = f3;
-					} else if (i === 60) {
-						fn = f4;
-					}
-					e = fn(a, b, c, d, e, block[i]);
-					b = ((b << 30) | (b >>> 2)) >>> 0;
-					// console.log("t=%s: %s  %s  %s  %s  %s", i,
-					//   hex8(e), hex8(a), hex8(b), hex8(c), hex8(d));
-					d = fn(e, a, b, c, d, block[i + 1]);
-					a = ((a << 30) | (a >>> 2)) >>> 0;
-					// console.log("t=%s: %s  %s  %s  %s  %s", i + 1,
-					//   hex8(d), hex8(e), hex8(a), hex8(b), hex8(c));
-					c = fn(d, e, a, b, c, block[i + 2]);
-					e = ((e << 30) | (e >>> 2)) >>> 0;
-					// console.log("t=%s: %s  %s  %s  %s  %s", i + 2,
-					//   hex8(c), hex8(d), hex8(e), hex8(a), hex8(b));
-					b = fn(c, d, e, a, b, block[i + 3]);
-					d = ((d << 30) | (d >>> 2)) >>> 0;
-					// console.log("t=%s: %s  %s  %s  %s  %s", i + 3,
-					//   hex8(b), hex8(c), hex8(d), hex8(e), hex8(a));
-					a = fn(b, c, d, e, a, block[i + 4]);
-					c = ((c << 30) | (c >>> 2)) >>> 0;
-					// console.log("t=%s: %s  %s  %s  %s  %s", i + 4,
-					//   hex8(a), hex8(b), hex8(c), hex8(d), hex8(e));
-				}
-
-				// console.log();
-				// process.stdout.write("H[0] = " + hex8(state[0]) + " + " + hex8(a));
-				state[0] += a;
-				// console.log(" = " + hex8(state[0]));
-				// process.stdout.write("H[1] = " + hex8(state[1]) + " + " + hex8(b));
-				state[1] += b;
-				// console.log(" = " + hex8(state[1]));
-				// process.stdout.write("H[2] = " + hex8(state[2]) + " + " + hex8(c));
-				state[2] += c;
-				// console.log(" = " + hex8(state[2]));
-				// process.stdout.write("H[3] = " + hex8(state[3]) + " + " + hex8(d));
-				state[3] += d;
-				// console.log(" = " + hex8(state[3]));
-				// process.stdout.write("H[4] = " + hex8(state[4]) + " + " + hex8(e));
-				state[4] += e;
-				// console.log(" = " + hex8(state[4]));
-
-			}
-
-			// input is a Uint8Array bitstream of the data
-			function sha1(input) {
-
-				var inputLength = input.length;
-
-				// Pad the input string length.
-				var length = inputLength + 9;
-				if (length % 64) {
-					length += 64 - (length % 64);
-				}
-
-				state[0] = 0x67452301;
-				state[1] = 0xefcdab89;
-				state[2] = 0x98badcfe;
-				state[3] = 0x10325476;
-				state[4] = 0xc3d2e1f0;
-
-				for (var offset = 0; offset < length; offset += 64) {
-
-					// Copy input to block and write padding as needed
-					for (var i = 0; i < 64; i++) {
-						var b = 0,
-							o = offset + i;
-						if (o < inputLength) {
-							b = input[o];
-						} else if (o === inputLength) {
-							b = 0x80;
-						} else {
-							// Write original bit length as a 64bit big-endian integer to the end.
-							var x = length - o - 1;
-							if (x >= 0 && x < 4) {
-								b = (inputLength << 3 >>> (x * 8)) & 0xff;
-							}
-						}
-
-						// Interpret the input bytes as big-endian per the spec
-						if (i % 4 === 0) {
-							block[i >> 2] = b << 24;
-						} else {
-							block[i >> 2] |= b << ((3 - (i % 4)) * 8);
-						}
-					}
-
-					// Extend the block
-					for (var i = 16; i < 80; i++) {
-						var w = block[i - 3] ^ block[i - 8] ^ block[i - 14] ^ block[i - 16];
-						block[i] = (w << 1) | (w >>> 31);
-					}
-
-					cycle(state, block);
-
-				}
-
-				// Swap the bytes around since they are big endian internally
-				return [
-					bstate[3], bstate[2], bstate[1], bstate[0],
-					bstate[7], bstate[6], bstate[5], bstate[4],
-					bstate[11], bstate[10], bstate[9], bstate[8],
-					bstate[15], bstate[14], bstate[13], bstate[12],
-					bstate[19], bstate[18], bstate[17], bstate[16],
-				]
-			}
-
-			return sha1;
-		});
 	}
 
 	function _registerDeferred() {
@@ -882,7 +684,6 @@
 	function __construct() {
 		_registerPolymer()
 		_polyfills()
-		_registerSha1()
 		_registerDeferred()
 		_registerAjax()
 		_registerServices()
